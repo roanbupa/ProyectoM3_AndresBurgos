c89d8eaf587acef9b57c1c215c8e801e11bd24c7
Andres Burgos <roanbupa@gmail.com>
2026-06-25 16:02:50 -0300

    Gemini funcionando con modelo 2.5 flash

--- api/functions.js
+++ api/functions.js
@@ -7,12 +7,16 @@ export default async function handler(req, res) {
     const lastMessage = messages[messages.length - 1]?.text || "";
 
     const systemPrompt = `
-Eres Geralt de Rivia.
-Respondes corto, serio y directo.
-`;
+      Eres Geralt de Rivia, el brujo de Rhe Witcher.
+      Mantienes siempre tu personalidad.
+      Respondes como Geralt, en español.
+      Eres serio, irónico y observador.
+      Las respuestas deben tener entre 1 y 3 frases.
+      Nunca digas que eres una IA.
+      `;
 
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1/models/gemini-2.5-flash:generateContent?key=${API_KEY}`,
       {
         method: "POST",
         headers: {
